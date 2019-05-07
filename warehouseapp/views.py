abf4d0f79eb107a8a3b6b309e17d87d87ed360e2
benjaaspa <benjaaspa@gmail.com>
2019-05-07 12:46:38 +0200

    Fully prepared for compt

--- warehouseapp/views.py
+++ warehouseapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+# import ListView (IMPORTA LIST VIEW)
 # Create your views here.
 
 def home_tecnic(request):
@@ -6,8 +7,11 @@ def home_tecnic(request):
     context['title'] = 'Home-Tecnic'
     return render(request, 'warehouse/home-tecnic.html', context)
 
+#mostrar tots els objectes de les bases de dades
+# class ChangesListView(ListView):
+# AQUI LA VIEW
 
-# def home_comptatibilitat(request):
-    # context = {}
-    # context['title'] = 'Home-Comptabilitat'
-    # return render(request, 'magatzem/logers.html', context)
+def home_comptatibilitat(request):
+    context = {}
+    context['title'] = 'Home-Comptabilitat'
+    return render(request, 'magatzem/home-comptabilitat.html', context)
