44a4679cb4d06a67c8040393f976204e9b1c43e1
Sactorum <parcheado2347@gmail.com>
2019-05-09 09:55:42 +0200

    Modified view cards products

--- warehouseapp/views.py
+++ warehouseapp/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-
-from warehouseapp.models import product
+from django.db import models
+from warehouseapp.models.product import Product
 from django.db.models import Q
 from django import forms
 
-# import ListView (IMPORTA LIST VIEW)
+from django.views.generic import ListView
 # Create your views here.
 
 def home_tecnic(request):
@@ -12,20 +12,28 @@ def home_tecnic(request):
     context['title'] = 'Home-Tecnic'
     return render(request, 'warehouse/home-tecnic.html', context)
 
-#mostrar tots els objectes de les bases de dades
-# class ChangesListView(ListView):
-# AQUI LA VIEW
-
 def home_comptatibilitat(request):
     context = {}
     context['title'] = 'Home-Comptabilitat'
     return render(request, 'magatzem/home-comptabilitat.html', context)
+
+class HomeTecnicList(ListView):
+    model = Product
+    context_object_name = 'product_list'
+    template_name = 'warehouse/home-tecnic.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Productes'
+        return context
+
+
 
 def view_cards_products(request):
     if 'q' in request.GET and request.GET['q']:
         q = request.GET['q']
         items = Product.objects.filter(model_icontains = q)
         return render(request, 'warehouse/view-cards-products.html',{'items':items, 'query': Q})
-
     else:
-        return HttpResponse('Please submit a search term.')
+        items = Product.objects.all()
+        return render(request, 'warehouse/view-cards-products.html', {'items': items})
